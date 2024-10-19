ceae519713f554802934f6e04f5d0a3dca6a6518
Omta <omtadayma2001@gmail.com>
2024-10-19 08:59:09 +0530

    "Updated MyAppointments and MyProfile components with new functionality and UI elements"

--- client/src/pages/MyAppointments.jsx
+++ client/src/pages/MyAppointments.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
 
 const MyAppointments = () => {
+  const {doctors} = useContext(AppContext)
   return (
-    <div>MyAppointments</div>
+    <div>
+      <p>My appointments</p>
+      <div>
+        {
+          doctors.slice(0,2).map((index,item)=>(
+            <div key={index}>
+              <div>
+                <img src={item.image} alt="" />
+              </div>
+            </div>
+          ))
+        }
+      </div>
+    </div>
   )
 }
 
