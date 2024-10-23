de4cef981a1146217256c2255ffdeb4b39c9cb24
Omta <omtadayma2001@gmail.com>
2024-10-23 16:49:45 +0530

    "Added admin context, doctor list page, and API for getting all doctors; updated admin controller and route"

--- server/routes/adminRoute.js
+++ server/routes/adminRoute.js
@@ -1,9 +1,12 @@
 import express, { Router } from "express";
-import { addDoctor, loginAdmin } from "../controllers/adminController.js";
+import { addDoctor, allDoctors, loginAdmin } from "../controllers/adminController.js";
 import upload from "../middlewares/multer.js";
 import authAdmin from "../middlewares/authAdmin.js";
 
 const adminRoute = express.Router();
 adminRoute.post("/add-doctor",authAdmin,upload.single("image"), addDoctor);
 adminRoute.post("/login",loginAdmin);
+adminRoute.post("/all-doctors",authAdmin,allDoctors);
+
+
 export default adminRoute;
