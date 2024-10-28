29dd1804f2da80f13824291f765168bb5248f30c
Omta <omtadayma2001@gmail.com>
2024-10-28 09:42:12 +0530

    "Added dashboard feature to admin panel, including API endpoint and frontend components"

--- server/routes/adminRoute.js
+++ server/routes/adminRoute.js
@@ -1,16 +1,23 @@
 import express from "express";
-import { addDoctor, allDoctors, appointmentCancel, appointmentsAdmin, loginAdmin } from "../controllers/adminController.js";
+import {
+  addDoctor,
+  adminDashboard,
+  allDoctors,
+  appointmentCancel,
+  appointmentsAdmin,
+  loginAdmin,
+} from "../controllers/adminController.js";
 import upload from "../middlewares/multer.js";
 import authAdmin from "../middlewares/authAdmin.js";
 import { changeAvailablity } from "../controllers/doctorController.js";
 
 const adminRoute = express.Router();
-adminRoute.post("/add-doctor",authAdmin,upload.single("image"), addDoctor);
-adminRoute.post("/login",loginAdmin);
-adminRoute.post("/all-doctors",authAdmin,allDoctors);
-adminRoute.post("/change-availability",authAdmin,changeAvailablity);
-adminRoute.get('/appointments',authAdmin,appointmentsAdmin);
-adminRoute.post('/cancel-appointment',authAdmin,appointmentCancel);
-
+adminRoute.post("/add-doctor", authAdmin, upload.single("image"), addDoctor);
+adminRoute.post("/login", loginAdmin);
+adminRoute.post("/all-doctors", authAdmin, allDoctors);
+adminRoute.post("/change-availability", authAdmin, changeAvailablity);
+adminRoute.get("/appointments", authAdmin, appointmentsAdmin);
+adminRoute.post("/cancel-appointment", authAdmin, appointmentCancel);
+adminRoute.get("/dashboard", authAdmin, adminDashboard);
 
 export default adminRoute;
